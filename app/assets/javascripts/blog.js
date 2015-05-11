a6d04627e52bce13805d61b0f258266ae1e06c19
garv123 <garv.mathur@gmail.com>
2015-05-11 18:50:57 +0530

    created backbone framework

---
+++ app/assets/javascripts/blog.js
@@ -0,0 +1,13 @@
+window.Blog = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Blog.initialize();
+});
