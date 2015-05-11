034106d6755475ea5969b031ceb7a8d6183803a1
garv123 <garv.mathur@gmail.com>
2015-05-11 19:38:18 +0530

    Getting proper response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    
+    user=User.new(name: params[:name],email: params[:email],password:params[:password],password_confirmation:params[:password_confirmation])
+    if user.save
+      render :json => user
+    else
+      render :json => {response:400,body:user.errors.full_messages}
+    end
   end
 
   def update
@@ -19,4 +26,10 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
